425381d7b3da5d0dd678c3345e33529a2326e9bd
Eagle-lucid <vicokefie@gmail.com>
2025-04-28 07:34:14 -0400

    feat: add a class to create a scroll UX.

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// Scroll behavior for header
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+})
