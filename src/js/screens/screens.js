954db761945f704c49980c7a48017239d4f154a4
Eagle-lucid <vicokefie@gmail.com>
2025-06-02 02:17:39 -0400

    feat: add files to create clear readibility and scalability

---
+++ src/js/screens/screens.js
@@ -0,0 +1,14 @@
+// Import all screens
+import { initScreen0 } from './screen0.js';
+import { initScreen1 } from './screen1.js';
+import { initScreen2 } from './screen2.js'; 
+import { initScreen3 } from './screen3.js';
+import { initScreen4 } from './screen4.js';
+
+export function initScreens() {
+    initScreen0();
+    initScreen1();
+    initScreen2();
+    initScreen3();
+    initScreen4();
+}
