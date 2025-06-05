79ef29bbb59b55c59dfc7bcbc731b1d61702de23
Eagle-lucid <vicokefie@gmail.com>
2025-06-05 02:33:48 -0400

    feat: integrate all screen animations into AllScreensAnimations class

--- src/js/screens/screens.js
+++ src/js/screens/screens.js
@@ -1,14 +1,18 @@
 // Import all screens
-import { initScreen0 } from './screen0.js';
-import { initScreen1 } from './screen1.js';
-import { initScreen2 } from './screen2.js'; 
-import { initScreen3 } from './screen3.js';
-import { initScreen4 } from './screen4.js';
+import { Screen0Animations } from './screen0.js';
+import { Screen1Animations } from './screen1.js';
+import { Screen2Animations } from './screen2.js'; 
+import { Screen3Animations } from './screen3.js';
+import { Screen4Animations } from './screen4.js';
 
-export function initScreens() {
-    initScreen0();
-    initScreen1();
-    initScreen2();
-    initScreen3();
-    initScreen4();
+export class AllScreensAnimations {
+    constructor() {
+        this.screens = {
+        screen0: new Screen0Animations(),
+        screen1: new Screen1Animations(),
+        screen2: new Screen2Animations(),
+        screen3: new Screen3Animations(),
+        screen4: new Screen4Animations()
+        }
+    }
 }
