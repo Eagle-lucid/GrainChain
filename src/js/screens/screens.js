734dedab9c224416bbb7579d39fb26a93dc42271
Eagle-lucid <vicokefie@gmail.com>
2025-06-27 02:12:02 -0400

    refactor: improve animation management system
    - Initialize screens in constructor
    - Add proper cleanup mechanism
    - Organize screen animations into dedicated classes

--- src/js/screens/screens.js
+++ src/js/screens/screens.js
@@ -7,12 +7,25 @@ import { Screen4Animations } from './screen4.js';
 
 export class AllScreensAnimations {
     constructor() {
-        this.screens = {
-        screen0: new Screen0Animations(),
-        screen1: new Screen1Animations(),
-        screen2: new Screen2Animations(),
-        screen3: new Screen3Animations(),
-        screen4: new Screen4Animations()
-        }
+        this.screens = {};
+        this.initScreens();
+    }
+
+    initScreens() {
+        this.screens.screen0 = new Screen0Animations();
+        this.screens.screen1 = new Screen1Animations();
+
+        this.screens.screen2 = new Screen2Animations();
+
+        this.screens.screen3 = new Screen3Animations();
+        this.screens.screen4 = new Screen4Animations();
+    }
+
+    cleanUp() {
+        Object.values(this.screens).forEach(screen => {
+            if (screen && typeof screen.cleanup === 'function') {
+                screen.cleanup();
+            }
+        });
     }
 }
